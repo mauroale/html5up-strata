351935ff047495c06a57e79bc05d06b43a78b371
Mauro <mauro.ale@hotmail.com>
2015-09-17 00:03:26 -0300

    fixing bug / starting development

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+ 	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+	    <link rel="icon" href="../../favicon.ico">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/assets/css/main.css' ?> " />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" />1<![endif]-->
+
+    	<title><?php bloginfo('name') ?></title>
+	</head>
+
+
+<!--
+<!DOCTYPE HTML>
+
+	Strata by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+
+		
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+
+<!--		
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri('/assets/css/main.css') ?> " />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	
+<!--	
+	<body id="top">
+
+		<!-- Header -->
+		<!--
+			<header id="header">
+				<a href="#" class="image avatar"><img src="<?php echo  get_template_directory_uri('images/avatar.jpg') ?>" alt="" /></a>
+				<h1><strong>I am Strata</strong>, a super simple<br />
+				responsive site template freebie<br />
+				crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+			</header>
+-->

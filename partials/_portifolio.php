8e36fa57cdb72133d1ba9e4be5b17938187b2a60
Mauro <mauro.ale@hotmail.com>
2016-01-05 23:05:11 -0200

    Ajustando modal da imagem do portifolio

--- partials/_portifolio.php
+++ partials/_portifolio.php
@@ -1,14 +1,21 @@
 <article class="6u 12u$(xsmall) work-item">
-	<a href="<?php echo  get_template_directory_uri() . 'images/fulls/01.jpg'?> " class="image fit thumb">
+	<a  class="image fit thumb" 
+		href="<?php 
+				 $thumb_url =
+				 wp_get_attachment_image_src( get_post_thumbnail_id() ,'thumbnail-size', true); 
+				 echo $thumb_url[0];
+		 	?>
+		 ">
 		<!--
 			<img src="<?php echo  get_template_directory_uri() . '/images/thumbs/01.jpg' ?> " alt="" />
 		-->
 	
 		<?php 	the_post_thumbnail(array(348,204)); ?>
+		
 	</a>
-
+<h3> <?php the_title() ?> </h3>	
 	<div class="work-item-text">
-		<h3> <?php the_title() ?> </h3>
+			
 		<p><?php the_content()  ?></p>
 	</div>
 </article>
--- sidebar.php
+++ sidebar.php
@@ -1,18 +1,18 @@
 <body id="top">
 
-		<!-- Header -->
-		
-			<header id="header">
-				<a href="#" class="image avatar"><img src="<?php echo  get_template_directory_uri() . '/images/avatar.jpg' ?>" alt="" /></a>
-				<h1>
-					<strong>Mauro Alexandrowitsch</strong>
-					
-					<br />
+	<!-- Header -->
+	
+	<header id="header">
+		<a href="#" class="image avatar"><img src="<?php echo  get_template_directory_uri() . '/images/avatar.jpg' ?>" alt="" /></a>
+		<h1>
+			<strong>Mauro Alexandrowitsch</strong>
+			
+			<br />
 
-					<?php echo get_custom('texto_sobre_mim') ?>
-				</h1>		
-			</header>
-		
-		<!-- Main -->
-			<div id="main">
+			<?php echo get_custom('texto_sobre_mim') ?>
+		</h1>		
+	</header>
+	
+	<!-- Main -->
+		<div id="main">
 
